82a51b44eac7758c6bfde20cc31b3880148ab59f
jason3e7 <jason3e7@gmail.com>
2018-11-29 22:24:34 -0800

    add 7

---
+++ 7.c
@@ -0,0 +1,9 @@
+int main() {
+  int a = 10;
+  if(a < 100) {
+    if(a >= 50) {
+      return 0;
+    }
+  }
+  return 0;
+}
---
+++ 7_1.c
@@ -0,0 +1,13 @@
+int main() {
+  int a = 1;
+  if(a > 2) {
+    if(a >= 2) {
+      if(a < 2) {
+        if(a <= 2) {
+          return 0;
+        }
+      }
+    }
+  }
+  return 0;
+}
