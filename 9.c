b300b259e0775db7194f9deb0a46d2a9523f7bfb
jason3e7 <jason3e7@gmail.com>
2018-12-03 18:35:14 -0800

    add 9

---
+++ 9.c
@@ -0,0 +1,19 @@
+int main() {
+  int a = 1;
+  char c;
+  switch (a) {
+    case 1 :
+      c = 'A';
+      break; 
+    case 2 :
+      c = 'B';
+      break; 
+    case 3 :
+      c = 'C';
+      break;
+    default: 
+      c = 'D';
+      break;
+  } 
+  return 0;
+}
