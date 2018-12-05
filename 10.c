84f73afb8c28ee996138c7700e53aea3e5697963
jason3e7 <jason3e7@gmail.com>
2018-12-05 01:21:22 -0800

    add 10

---
+++ 10.c
@@ -0,0 +1,10 @@
+int main() {
+  int a, b;
+  a = 0;
+  b = 0;
+  while(b < 10) {
+    a += b;
+    b++;
+  }
+  return 0;
+}
---
+++ 10_1.c
@@ -0,0 +1,9 @@
+int main() {
+  int a, i;
+  //int i, a;
+  a = 0;
+  for(i = 0; i < 10; i++) {
+    a += i;
+  }
+  return 0;
+}
